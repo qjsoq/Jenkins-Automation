800fc55698c84bb08b9f3638644440c301e2e8da
qjsoq <artamonovdima355@gmail.com>
2025-06-19 21:03:52 +0000

    Update app pipeline

--- jenkins/application/app_pipeline.groovy
+++ jenkins/application/app_pipeline.groovy
@@ -1,7 +1,10 @@
 def call() {
     
-    stage('Application Build') {
-        echo 'Building application...'
+    stage('Clone Config Repo') {
+        dir('config') {
+            git credentialsId: 'ssh_privatekey_github', url: 'git@github.com:iviul/Config.git'
+            sh 'ls -la'
+        }
     }
 }
 return this
