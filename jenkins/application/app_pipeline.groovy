36b43cbb7a12d1dece493fd878fd789b6c12061b
qjsoq <artamonovdima355@gmail.com>
2025-06-19 21:43:37 +0000

    Update app pipeline

--- jenkins/application/app_pipeline.groovy
+++ jenkins/application/app_pipeline.groovy
@@ -1,8 +1,12 @@
 def call() {
     
-    stage('Clone Config Repo') {
+    stage('Clone Repositories') {
         dir('config') {
-            git branch: 'main', credentialsId: 'ssh_privatekey_github', url: 'git@github.com:iviul/Config.git'
+            git branch: 'DI-34-Develop', credentialsId: 'ssh_privatekey_github', url: 'git@github.com:iviul/Config.git'
+            sh 'ls -la'
+        }
+        dir('milestone') {
+            git branch 'main', url: 'https://github.com/iviul/Milestone-2.git'
             sh 'ls -la'
         }
     }
