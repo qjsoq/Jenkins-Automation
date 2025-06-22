ce86a539dca2fdde63e8b02012f60799d60628e6
Artamonov Dima <104785233+qjsoq@users.noreply.github.com>
2025-06-22 23:49:52 +0300

    Update app_pipeline.groovy

--- jenkins/application/app_pipeline.groovy
+++ jenkins/application/app_pipeline.groovy
@@ -22,10 +22,12 @@ def call() {
                     dir('config') {
                         git branch: 'DI-34-Develop', credentialsId: 'ssh_privatekey_github', url: 'git@github.com:iviul/Config.git'
                         sh 'ls -la'
+                        sh 'pwd'
                     }
                     dir('milestone') {
                         git branch: 'main', url: 'https://github.com/iviul/Milestone-2.git'
                         sh 'ls -la'
+                        sh 'pwd'
                     }
                 }
 
